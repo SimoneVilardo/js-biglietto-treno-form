04b360b832bc3cccf6acd08b0bc16aa38212592e
SimoneVilardo <simovila95@gmail.com>
2023-05-19 09:56:56 +0200

    TOLTI I CONSOLE LOG E ULTIME MODIFICHE

--- js/script.js
+++ js/script.js
@@ -7,13 +7,10 @@ let gen_button = document.getElementById('genera');
 gen_button.addEventListener('click', function(){
 
     let nome_cognome = document.getElementById('nome').value;
-    console.log(nome_cognome)
 
-    let percorso_utente = document.getElementById('percorso').value;
-    console.log(percorso_utente)
+    let percorso_utente = parseInt(document.getElementById('percorso').value);
 
     let fascia_eta = document.getElementById('eta').value;
-    console.log(fascia_eta)
 // VARIABILE CARROZZA
     let carrozza = Math.floor(Math.random() * 10 + 1)
 
@@ -21,10 +18,9 @@ gen_button.addEventListener('click', function(){
     let codice_cp = Math.floor(Math.random() * 100000 + 1)
 
 // SCONTO MINORE
-    if (fascia_eta == 'Minorenne') {
+    if (fascia_eta === 'Minorenne') {
 
         let prezzo = (percorso_utente * costo_base * 0.8);
-        console.log(prezzo)
         
         document.getElementById('nome-passeggero').innerHTML = `${nome_cognome}`
         document.getElementById('offerta').innerHTML = `${fascia_eta}`
@@ -33,7 +29,7 @@ gen_button.addEventListener('click', function(){
         document.getElementById('costo-biglietto').innerHTML = `${prezzo.toFixed(2)}€`
 
 // SCONTO OVER 65        
-    } else if (fascia_eta == 'Over65') {
+    } else if (fascia_eta === 'Over65') {
 
         let prezzo = (percorso_utente * costo_base * 0.4);
 
@@ -44,7 +40,7 @@ gen_button.addEventListener('click', function(){
         document.getElementById('costo-biglietto').innerHTML = `${prezzo.toFixed(2)}€`
 
 // PREZZO STANDARD        
-    } else (fascia_eta == 'Maggiorenne') ;{
+    } else (fascia_eta === 'Maggiorenne') ;{
 
         let prezzo = (percorso_utente * costo_base);
 
@@ -56,41 +52,4 @@ gen_button.addEventListener('click', function(){
 
     }
 
-
 })
-
-
-
-
-
-
-
-
-// // totale biglietto
-// let totale = (kilometri * costo_base);
-
-// //eta utente
-
-
-// let sconto;
-
-// let prezzo_scontato;
-
-
-// // condizioni + risultato in pagina
-// if (eta < 18) {
-//     sconto = (totale * 20 / 100);
-
-//     prezzo_scontato = (totale - sconto);
-
-//     document.getElementById('prezzo_finale').innerHTML = prezzo_scontato.toFixed(2) + '€';
-
-// } else if ( eta >= 65) {
-//         sconto = (totale * 40 / 100);
-    
-//         prezzo_scontato = (totale - sconto);
-
-//         document.getElementById('prezzo_finale').innerHTML = prezzo_scontato.toFixed(2) + '€';
-// } else {
-//     document.getElementById('prezzo_finale').innerHTML = totale.toFixed(2) + '€';
-// }
